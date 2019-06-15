9b9f5af802a70107ea5b1e08601445489c28c83f
Ronen Tal <Rtal@digitalclues.com>
2019-06-15 18:56:32 +0300

    Sending XML Metrics

--- csv2xml/Go-Xml.sh
+++ csv2xml/Go-Xml.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# ---====--- Sending XML Alert File ---===----
+# ---====--- Sending XML Alert File using csv2xml.awk ---===----
 # awk -F ',' -v node=Bad-Proxy -f csv2xml.awk Bad-Proxy.csv
+
+#---===--- Find Metrics File Name Without Extention ---===---
+# Metric file name example Bad-Proxy.csv
+# -----------------------------------------------------------
 
 fullfile=$1
 fname=$(basename $fullfile)
 fbname=${fname%.*}
-node=$fbname
 
-echo "awk -F ',' -v node="$fbname" -f csv2xml.awk2" $1
-awk -F ',' -v node=$fbname -f csv2xml.awk2 $1
+#---===--- Get Metric Filename Before "-" ---===---
+Item1="$(echo ${fname} | awk -F '[-]' '{print $1}')"
+#echo "awk -F ',' -v node="$fbname" -f csv2xml.awk2" $1
+
+#---===--- Create The XMl File ---===---
+awk -F ',' -v MasterXML=$fbname -v Item=$Item1 -f csv2xml.awk2 $1
