3ec9fe2df0d4d041ec6df58ae9b29e731ac284f4
Ronen Tal <Rtal@digitalclues.com>
2019-06-28 10:38:10 +0300

    Last Line was Deleted

--- csv2xml/Go-Xml.sh
+++ csv2xml/Go-Xml.sh
@@ -28,11 +28,6 @@ Item1="$(echo ${fname} | awk -F '[-]' '{print $1}')"
 
 #echo "awk -F ',' -v node="$fbname" -f csv2xml.awk2" $1
 
-<<<<<<< HEAD
 #-----------         Generate The XMl Output          ----------
 awk -F ',' -v MasterXML=$fbname -v Item=$Item1 -f csv2xml.awk2 $1
 
-=======
-#---===--- Create The XMl File ---===---
-awk -F ',' -v MasterXML=$fbname -v Item=$Item1 -f csv2xml.awk2 $1
->>>>>>> 4896a5a10599bb0b558114027453579d9d32fecc
