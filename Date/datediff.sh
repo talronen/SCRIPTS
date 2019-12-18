d32e88c4eee073740a2c99de10b08c202b65149f
Ronen Tal <Rtal@digitalclues.com>
2019-12-18 20:22:17 +0200

    Calculate Diff between tow Dates

---
+++ Date/datediff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will calculated Differance between tow Dates in Days
+#
+# Syntx: ./datediff.sh '2019/1/10' '2019/1/1'
+#
+
+#datediff() {
+#    d1=$(date -d "$1" +%s)
+#    d2=$(date -d "$2" +%s)
+#    echo $(( (d1 - d2) / 86400 )) days
+#}
+
+echo  $((($(date -d $1 +%s) - $(date -d $2 +%s)) / 86400))
+
+
+
